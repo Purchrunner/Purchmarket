91da12c9832c772e8047507dee90139bbf0b05af
Tomas Andersson <j.tomas.andersson@gmail.com>
2023-08-31 13:25:08 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,6 +26,60 @@ return (
     <main className={`pt-20 ${lato.variable} font-sans`}>
       <Layout>
         {/* Google tag (gtag.js) */}
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-DCHPCF2P9B"
+          />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+          gtag('config', 'G-DCHPCF2P9B');
+          `}
+        </Script>
+      <Component {...pageProps} />
+      </Layout>
+    </main>
+  </ApolloProvider>
+</SessionProvider>
+);
+}
+
+
+
+
+export default MyApp;
+
+/*
+import { AppProps } from "next/app";
+import { Lato } from "@next/font/google";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../lib/apolloClient";
+import { AuthProvider } from "../hooks/useAuth";
+import Layout from "../components/layout";
+import "../styles/index.css";
+import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
+
+
+const lato = Lato({
+weight: ["400", "700", "900"],
+style: ["normal", "italic"],
+subsets: ["latin"],
+variable: "--font-lato",
+});
+
+
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+return (
+<SessionProvider session={pageProps.session}>
+  <ApolloProvider client={client}>
+    <main className={`pt-20 ${lato.variable} font-sans`}>
+      <Layout>
+        {/* Google tag (gtag.js) */}
         <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-DCHPCF2P9B"
@@ -49,4 +103,4 @@ return (
 
 
 
-export default MyApp;
+export default MyApp;*/
