a03c8733cce7df6f403f41df91c589fdaf1fec68
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2023-09-04 18:19:35 +0200

    Update GA script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,35 +5,40 @@ import { client } from "../lib/apolloClient";
 import { AuthProvider } from "../hooks/useAuth";
 import Layout from "../components/layout";
 import "../styles/index.css";
-import { useEffect } from "react";
-import { initGA, logPageView } from "../lib/ga";
+import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
 
 const lato = Lato({
-  weight: ["400", "700", "900"],
-  style: ["normal", "italic"],
-  subsets: ["latin"],
-  variable: "--font-lato",
+weight: ["400", "700", "900"],
+style: ["normal", "italic"],
+subsets: ["latin"],
+variable: "--font-lato",
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if (!(window as any).GA_INITIALIZED) {
-      initGA();
-      (window as any).GA_INITIALIZED = true;
-    }
-    logPageView();
-  }, []);
-
   return (
+  <SessionProvider session={pageProps.session}>
     <ApolloProvider client={client}>
-      <AuthProvider>
-        <main className={`pt-20 ${lato.variable} font-sans`}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </main>
-      </AuthProvider>
+      <main className={`pt-20 ${lato.variable} font-sans`}>
+        <Layout>
+          {/* Google tag (gtag.js) */}
+            <Script
+              strategy="afterInteractive"
+              src="https://www.googletagmanager.com/gtag/js?id=G-DCHPCF2P9B"
+            />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+            gtag('config', 'G-DCHPCF2P9B');
+            `}
+          </Script>
+        <Component {...pageProps} />
+        </Layout>
+      </main>
     </ApolloProvider>
+  </SessionProvider>
   );
 }
 
